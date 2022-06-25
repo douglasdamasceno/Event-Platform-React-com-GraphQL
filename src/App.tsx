03235728f0496fe05176df03bbe66b1c11ad8d8b
Douglas Damasceno <douglasmdamasceno@gmail.com>
2022-06-24 23:21:50 -0300

    primeira query com graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,29 @@
+import { gql } from "@apollo/client"
+import { useEffect } from "react"
+import { client } from "./lib/apollo"
+
+const GET_LESSONS_QUERY = gql`
+  query{
+    lessons{
+      id
+      title
+
+      teacher{
+        name
+      }
+    }
+  }
+`
 function App() {
+  useEffect(() => {
+    client.query({
+      query:GET_LESSONS_QUERY,
+    }).then(response => {
+      console.log(response)
+    }).catch(error => {
+      console.log(error)
+    })  
+  }, [])
   
   return (
     <div>
